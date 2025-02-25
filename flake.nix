5ba0f3f894d8bbe899a29a7e7fd4223e3f71e9e1
titus <titus.huebner@gmail.com>
2025-02-25 11:33:08 +0100

    setup nix development flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development Environment for VinylShelf";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux"; 
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.dotnet-sdk_8   
+        pkgs.dotnet-runtime
+	# entity framework command line utils
+	pkgs.dotnet-ef
+	pkgs.zsh
+      ];
+
+      shellHook = ''
+	exec zsh
+      '';
+    };
+  };
+}
