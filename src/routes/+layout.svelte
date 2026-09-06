cfa575cef44cfc46d680299943578bb7112909c7
titus <me@titusio.net>
2026-09-06 06:36:37 +0200

    scaffold sveltekit project with tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.svg";
+
+    let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav>
+    <div class="flex md:flex md:grow flex-row-reverse space-x-1 p-4">
+        <a class="p-3 font-bold" href="/demo/better-auth">Login</a>
+        <a class="p-3 font-bold" href="/library">Library</a>
+    </div>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="p-5 place-items-center grid">
+    <h1 class="text-xl font-bold">Vinyl Shelf</h1>
+    <p>A digital home for your records.</p>
+</div>
